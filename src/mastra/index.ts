31aca9789eeea1f059be3639854f8d375a000132
liujian <liujian@zaxh.cn>
2025-05-21 17:26:55 +0800

    feat: 添加cloudflare

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,10 +1,23 @@
+/** @format */
 
-import { Mastra } from '@mastra/core/mastra';
-import { createLogger } from '@mastra/core/logger';
-import { LibSQLStore } from '@mastra/libsql';
+import { Mastra } from "@mastra/core/mastra";
+import { createLogger } from "@mastra/core/logger";
+import { LibSQLStore } from "@mastra/libsql";
+import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
+import dotenv from "dotenv";
+dotenv.config();
+import { weatherAgent } from "./agents";
 
-import { weatherAgent } from './agents';
+// 从环境变量获取敏感信息
+const apiToken = process.env.CLOUDFLARE_API_TOKEN;
+const apiEmail = process.env.CLOUDFLARE_EMAIL;
+const scope = process.env.CLOUDFLARE_SCOPE;
 
+if (!apiToken || !apiEmail || !scope) {
+  throw new Error("Missing Cloudflare API token or email");
+}
+
+console.log("apiToken", apiToken);
 export const mastra = new Mastra({
   agents: { weatherAgent },
   storage: new LibSQLStore({
@@ -12,7 +25,16 @@ export const mastra = new Mastra({
     url: ":memory:",
   }),
   logger: createLogger({
-    name: 'Mastra',
-    level: 'info',
+    name: "Mastra",
+    level: "info",
+  }),
+  deployer: new CloudflareDeployer({
+    scope,
+    projectName: "my-mastra-app",
+    workerNamespace: "my-mastra-app",
+    auth: {
+      apiToken,
+      apiEmail,
+    },
   }),
 });
